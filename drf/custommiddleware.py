8fcd06b0c650fd66c3461481e06ef158b1cabb1c
Kuntal Samanta <39960418+kuntal-python-django@users.noreply.github.com>
2020-10-03 16:44:23 +0530

    Update custommiddleware.py

--- drf/custommiddleware.py
+++ drf/custommiddleware.py
@@ -10,3 +10,17 @@ class KuntalDemoMiddleware(MiddlewareMixin):
         print("###############################")
         print('\n', '          My Middleware         ', '\n')
         print("###############################")
+        # print(request.META)
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        print("Your Ip is : ", ip)
+
+        ip_list = ['172.10.123.1', '172.10.123.2', '172.10.123.3', '127.0.0.1']
+
+        if ip in ip_list:
+            print("Your Are Welcome")
+        else:
+            print("!!!!!!!!!!!!!!!!!!!") 
